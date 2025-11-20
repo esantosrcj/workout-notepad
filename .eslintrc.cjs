7873f833ea7067328b0bd98e8f446045edc59f78
Lalo Santos <esantosrcj@gmail.com>
2025-11-20 15:18:04 -0600

    Update project config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier',
+		'plugin:prettier-plugin-organize-imports',
+		'plugin:prettier-plugin-svelte',
+		'plugin:prettier-plugin-tailwindcss'
+	],
+	plugins: ['@typescript-eslint'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+	rules: {
+		'no-console': 'warn'
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
